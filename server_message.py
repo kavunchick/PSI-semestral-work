5e69ce45c0e5f20a6f1b36c77f0bffe16916d461
VladHiliaka <127258341+VladHiliaka@users.noreply.github.com>
2023-03-10 20:16:00 +0100

    Update server_message.py

--- server_message.py
+++ server_message.py
@@ -1,10 +1,12 @@
 import additional_function as ad
+import errno
+
 
 KEYS = {0: (23019, 32037), 1: (32037, 29295), 2: (18789, 13603), 3: (16443, 29533), 4: (18189, 21952)}
 
 
-def SERVER_CONFIRMATION(socket, name, key_id):
-    number = ad.hashName(name) + int(KEYS[key_id][0]) % 65536
+def SERVER_CONFIRMATION(socket, name, keyID):
+    number = ad.hashName(name) + int(KEYS[keyID][0]) % 65536
     answer = f'{number}\a\b'
     socket.send(answer.encode())
 
@@ -21,11 +23,13 @@ def SERVER_SYNTAX_ERROR(socket):
 
 def ACCEPT_CLIENT_KEY(socket, name, key_id):
     number = (ad.hashName(name) + KEYS[key_id][1]) % 65536
-    clientID = socket.recv(1024)
-    clientID_decode = clientID.decode()
-    index = clientID_decode.find('\a\b')
-    clientID_decode = int(clientID_decode[:index])
-    if clientID_decode == number:
+    if ad.messagesArray == []:
+        ad.receiveMessage(socket)
+        clientID = ad.messagesArray.pop(0).decode()
+    else:
+        clientID = ad.messagesArray.pop(0).decode()
+    clientID = int(clientID)
+    if clientID == number:
         socket.send(b'200 OK\a\b')
     else:
         socket.send(b'300 LOGIN FAILED\a\b')
@@ -45,18 +49,21 @@ def SERVER_TURN_RIGHT(socket):
 
 
 def ACCEPT_CLIENT_USERNAME(socket):
-    BotName = socket.recv(1024)
-    BotName_decode = BotName.decode()
-    index = BotName_decode.find('\a\b')
-    return BotName_decode[:index], BotName
+    ad.receiveMessage(socket)
+    BotName = ad.messagesArray.pop(0).decode()
+    return BotName
 
 
 def SERVER_KEY_REQUEST(socket):
     socket.send(b'107 KEY REQUEST\a\b')
-    KeyID = socket.recv(1024)
-    if not ad.decodeMessage(KeyID).isnumeric():
+    if ad.messagesArray == []:
+        ad.receiveMessage(socket)
+        KeyID = ad.messagesArray.pop(0).decode()
+    else:
+        KeyID = ad.messagesArray.pop(0).decode()
+    if not KeyID.isnumeric():
         SERVER_SYNTAX_ERROR(socket)
-    KeyID = int(ad.decodeMessage(KeyID))
+    KeyID = int(KeyID)
     if KeyID > 4 or KeyID < 0:
         SERVER_KEY_OUT_OF_RANGE_ERROR(socket)
     return KeyID
