cf8e5ecb44dc234fa2d7be7037b7f3356937b0f5
VladHiliaka <127258341+VladHiliaka@users.noreply.github.com>
2023-03-19 21:39:13 +0100

    Update additional_function.py

--- additional_function.py
+++ additional_function.py
@@ -3,13 +3,23 @@ import server_message as sm
 messagesArray = []
 
 
-def receiveMessage(socket):
+def receiveMessage(socket, stage=None):
     global messagesArray
     pattern = b'\a\b'
     message = b''
     socket.settimeout(1)
     try:
         data = socket.recv(1024)
+        smth = data.split(b'\a\b')
+        if stage == 'ACCEPT_CLIENT_USERNAME':
+            if len(smth[0]) > 16:
+                sm.SERVER_SYNTAX_ERROR(socket)
+        elif stage == 'SERVER_PICK_UP':
+            if len(smth[0]) > 96:
+                sm.SERVER_SYNTAX_ERROR(socket)
+        elif stage == 'coordinates':
+            if len(smth[0]) > 8:
+                sm.SERVER_SYNTAX_ERROR(socket)
     except:
         socket.close()
     message += data
@@ -35,7 +45,6 @@ def decodeMessage(message):
 
 
 def defineDirection(tup1, tup2):
-    print(tup1, tup2)
     if tup1[1] == tup2[1]:
         if tup1[0] > tup2[0]:
             return 'L'
@@ -48,39 +57,150 @@ def defineDirection(tup1, tup2):
             return 'U'
 
 
+def obstacles(socket, direction=None, coordinates=None):
+    if direction == 'R':
+        if coordinates[1] < 0:
+            sm.SERVER_TURN_LEFT(socket)
+            if not messagesArray:
+                receiveMessage(socket, 'coordinates')
+                messagesArray.pop(0)
+            else:
+                messagesArray.pop(0).decode()
+            sm.SERVER_MOVE(socket)
+            if not messagesArray:
+                receiveMessage(socket, 'coordinates')
+                messagesArray.pop(0)
+            else:
+                messagesArray.pop(0).decode()
+            sm.SERVER_TURN_RIGHT(socket)
+            if not messagesArray:
+                receiveMessage(socket, 'coordinates')
+                messagesArray.pop(0)
+            else:
+                messagesArray.pop(0).decode()
+        elif coordinates[1] > 0:
+            sm.SERVER_TURN_RIGHT(socket)
+            if not messagesArray:
+                receiveMessage(socket, 'coordinates')
+                messagesArray.pop(0)
+            else:
+                messagesArray.pop(0).decode()
+            sm.SERVER_MOVE(socket)
+            if not messagesArray:
+                receiveMessage(socket, 'coordinates')
+                messagesArray.pop(0)
+            else:
+                messagesArray.pop(0).decode()
+            sm.SERVER_TURN_LEFT(socket)
+            if not messagesArray:
+                receiveMessage(socket, 'coordinates')
+                messagesArray.pop(0)
+            else:
+                messagesArray.pop(0).decode()
+    elif direction == 'L':
+        if coordinates[1] > 0:
+            sm.SERVER_TURN_LEFT(socket)
+            if not messagesArray:
+                receiveMessage(socket, 'coordinates')
+                messagesArray.pop(0)
+            else:
+                messagesArray.pop(0).decode()
+            sm.SERVER_MOVE(socket)
+            if not messagesArray:
+                receiveMessage(socket, 'coordinates')
+                messagesArray.pop(0)
+            else:
+                messagesArray.pop(0).decode()
+            sm.SERVER_TURN_RIGHT(socket)
+            if not messagesArray:
+                receiveMessage(socket, 'coordinates')
+                messagesArray.pop(0)
+            else:
+                messagesArray.pop(0).decode()
+        elif coordinates[1] < 0:
+            sm.SERVER_TURN_RIGHT(socket)
+            if not messagesArray:
+                receiveMessage(socket, 'coordinates')
+                messagesArray.pop(0)
+            else:
+                messagesArray.pop(0).decode()
+            sm.SERVER_MOVE(socket)
+            if not messagesArray:
+                receiveMessage(socket, 'coordinates')
+                messagesArray.pop(0)
+            else:
+                messagesArray.pop(0).decode()
+            sm.SERVER_TURN_LEFT(socket)
+            if not messagesArray:
+                receiveMessage(socket, 'coordinates')
+                messagesArray.pop(0)
+            else:
+                messagesArray.pop(0).decode()
+    else:
+        sm.SERVER_TURN_RIGHT(socket)
+        if not messagesArray:
+            receiveMessage(socket, 'coordinates')
+            messagesArray.pop(0)
+        else:
+            messagesArray.pop(0).decode()
+        sm.SERVER_MOVE(socket)
+        if not messagesArray:
+            receiveMessage(socket, 'coordinates')
+            messagesArray.pop(0)
+        else:
+            messagesArray.pop(0).decode()
+        sm.SERVER_TURN_LEFT(socket)
+        if not messagesArray:
+            receiveMessage(socket, 'coordinates')
+            messagesArray.pop(0)
+        else:
+            messagesArray.pop(0).decode()
+    sm.SERVER_MOVE(socket)
+    if not messagesArray:
+        receiveMessage(socket, 'coordinates')
+        coordinates = recieveCoordinate(messagesArray.pop(0).decode(), socket)
+    else:
+        coordinates = recieveCoordinate(messagesArray.pop(0).decode(), socket)
+    sm.SERVER_MOVE(socket)
+    if not messagesArray:
+        receiveMessage(socket, 'coordinates')
+        coordinates2 = recieveCoordinate(messagesArray.pop(0).decode(), socket)
+    else:
+        coordinates2 = recieveCoordinate(messagesArray.pop(0).decode(), socket)
+    return defineDirection(coordinates, coordinates2), coordinates2
+
+
 def hashName(name):
     return (sum(ord(c) for c in name) * 1000) % 65536
 
 
-def recieveCoordinate(string):
+def recieveCoordinate(string, socket):
+    if '.' in string or string[-1].isspace():
+        sm.SERVER_SYNTAX_ERROR(socket)
     return tuple(map(int, string.split()[1:]))
 
 
 def algorithm(socket, coordinates=None, direction=None):
     if coordinates == (0, 0):
-        socket.send(b'105 GET MESSAGE\a\b')
-        if not messagesArray:
-            receiveMessage(socket)
-            messagesArray.pop(0)
-        else:
-            messagesArray.pop(0).decode()
-        socket.send(b'106 LOGOUT\a\b')
-        socket.close()
+        sm.SERVER_PICK_UP(socket)
 
     if direction is None:
         sm.SERVER_MOVE(socket)
         if not messagesArray:
-            receiveMessage(socket)
-            coordinates = recieveCoordinate(messagesArray.pop(0).decode())
+            receiveMessage(socket, 'coordinates')
+            coordinates = recieveCoordinate(messagesArray.pop(0).decode(), socket)
         else:
-            coordinates = recieveCoordinate(messagesArray.pop(0).decode())
+            coordinates = recieveCoordinate(messagesArray.pop(0).decode(), socket)
         sm.SERVER_MOVE(socket)
         if not messagesArray:
-            receiveMessage(socket)
-            coordinates2 = recieveCoordinate(messagesArray.pop(0).decode())
+            receiveMessage(socket, 'coordinates')
+            coordinates2 = recieveCoordinate(messagesArray.pop(0).decode(), socket)
         else:
-            coordinates2 = recieveCoordinate(messagesArray.pop(0).decode())
-        direction = defineDirection(coordinates, coordinates2)
+            coordinates2 = recieveCoordinate(messagesArray.pop(0).decode(), socket)
+        if coordinates == coordinates2:
+            direction, coordinates2 = obstacles(socket)
+        else:
+            direction = defineDirection(coordinates, coordinates2)
         algorithm(socket, coordinates2, direction)
 
     if coordinates[0] != 0:
@@ -88,21 +208,23 @@ def algorithm(socket, coordinates=None, direction=None):
             if coordinates[0] < 0:
                 sm.SERVER_MOVE(socket)
                 if not messagesArray:
-                    receiveMessage(socket)
-                    coordinates = recieveCoordinate(messagesArray.pop(0).decode())
+                    receiveMessage(socket, 'coordinates')
+                    coordinates2 = recieveCoordinate(messagesArray.pop(0).decode(), socket)
                 else:
-                    coordinates = recieveCoordinate(messagesArray.pop(0).decode())
-                algorithm(socket, coordinates, direction)
+                    coordinates2 = recieveCoordinate(messagesArray.pop(0).decode(), socket)
+                if coordinates == coordinates2:
+                    direction, coordinates2 = obstacles(socket)
+                algorithm(socket, coordinates2, direction)
             else:
                 sm.SERVER_TURN_RIGHT(socket)
                 if not messagesArray:
-                    receiveMessage(socket)
+                    receiveMessage(socket, 'coordinates')
                     messagesArray.pop(0)
                 else:
                     messagesArray.pop(0).decode()
                 sm.SERVER_TURN_RIGHT(socket)
                 if not messagesArray:
-                    receiveMessage(socket)
+                    receiveMessage(socket, 'coordinates')
                     messagesArray.pop(0)
                 else:
                     messagesArray.pop(0).decode()
@@ -112,13 +234,13 @@ def algorithm(socket, coordinates=None, direction=None):
             if coordinates[0] < 0:
                 sm.SERVER_TURN_RIGHT(socket)
                 if not messagesArray:
-                    receiveMessage(socket)
+                    receiveMessage(socket, 'coordinates')
                     messagesArray.pop(0)
                 else:
                     messagesArray.pop(0).decode()
                 sm.SERVER_TURN_RIGHT(socket)
                 if not messagesArray:
-                    receiveMessage(socket)
+                    receiveMessage(socket, 'coordinates')
                     messagesArray.pop(0)
                 else:
                     messagesArray.pop(0).decode()
@@ -126,46 +248,49 @@ def algorithm(socket, coordinates=None, direction=None):
             else:
                 sm.SERVER_MOVE(socket)
                 if not messagesArray:
-                    receiveMessage(socket)
-                    coordinates = recieveCoordinate(messagesArray.pop(0).decode())
+                    receiveMessage(socket, 'coordinates')
+                    coordinates2 = recieveCoordinate(messagesArray.pop(0).decode(), socket)
                 else:
-                    coordinates = recieveCoordinate(messagesArray.pop(0).decode())
-                algorithm(socket, coordinates, direction)
+                    coordinates2 = recieveCoordinate(messagesArray.pop(0).decode(), socket)
+                if coordinates == coordinates2:
+                    direction, coordinates2 = obstacles(socket)
+                algorithm(socket, coordinates2, direction)
+                algorithm(socket, coordinates2, direction)
 
         if direction == 'U':
             if coordinates[0] < 0:
                 sm.SERVER_TURN_RIGHT(socket)
                 if not messagesArray:
-                    receiveMessage(socket)
-                    coordinates = recieveCoordinate(messagesArray.pop(0).decode())
+                    receiveMessage(socket, 'coordinates')
+                    coordinates = recieveCoordinate(messagesArray.pop(0).decode(), socket)
                 else:
-                    coordinates = recieveCoordinate(messagesArray.pop(0).decode())
+                    coordinates = recieveCoordinate(messagesArray.pop(0).decode(), socket)
                 algorithm(socket, coordinates, 'R')
             else:
                 sm.SERVER_TURN_LEFT(socket)
                 if not messagesArray:
-                    receiveMessage(socket)
-                    coordinates = recieveCoordinate(messagesArray.pop(0).decode())
+                    receiveMessage(socket, 'coordinates')
+                    coordinates = recieveCoordinate(messagesArray.pop(0).decode(), socket)
                 else:
-                    coordinates = recieveCoordinate(messagesArray.pop(0).decode())
+                    coordinates = recieveCoordinate(messagesArray.pop(0).decode(), socket)
                 algorithm(socket, coordinates, 'L')
 
         if direction == 'D':
             if coordinates[0] < 0:
                 sm.SERVER_TURN_LEFT(socket)
                 if not messagesArray:
-                    receiveMessage(socket)
-                    coordinates = recieveCoordinate(messagesArray.pop(0).decode())
+                    receiveMessage(socket, 'coordinates')
+                    coordinates = recieveCoordinate(messagesArray.pop(0).decode(), socket)
                 else:
-                    coordinates = recieveCoordinate(messagesArray.pop(0).decode())
+                    coordinates = recieveCoordinate(messagesArray.pop(0).decode(), socket)
                 algorithm(socket, coordinates, 'R')
             else:
                 sm.SERVER_TURN_RIGHT(socket)
                 if not messagesArray:
-                    receiveMessage(socket)
-                    coordinates = recieveCoordinate(messagesArray.pop(0).decode())
+                    receiveMessage(socket, 'coordinates')
+                    coordinates = recieveCoordinate(messagesArray.pop(0).decode(), socket)
                 else:
-                    coordinates = recieveCoordinate(messagesArray.pop(0).decode())
+                    coordinates = recieveCoordinate(messagesArray.pop(0).decode(), socket)
                 algorithm(socket, coordinates, 'L')
 
     if coordinates[1] != 0:
@@ -173,82 +298,87 @@ def algorithm(socket, coordinates=None, direction=None):
             if coordinates[1] < 0:
                 sm.SERVER_TURN_LEFT(socket)
                 if not messagesArray:
-                    receiveMessage(socket)
-                    recieveCoordinate(messagesArray.pop(0).decode())
+                    receiveMessage(socket, 'coordinates')
+                    recieveCoordinate(messagesArray.pop(0).decode(), socket)
                 else:
-                    recieveCoordinate(messagesArray.pop(0).decode())
+                    recieveCoordinate(messagesArray.pop(0).decode(), socket)
                 algorithm(socket, coordinates, 'U')
             else:
                 sm.SERVER_TURN_RIGHT(socket)
                 if not messagesArray:
-                    receiveMessage(socket)
-                    recieveCoordinate(messagesArray.pop(0).decode())
+                    receiveMessage(socket, 'coordinates')
+                    recieveCoordinate(messagesArray.pop(0).decode(), socket)
                 else:
-                    recieveCoordinate(messagesArray.pop(0).decode())
+                    recieveCoordinate(messagesArray.pop(0).decode(), socket)
                 algorithm(socket, coordinates, 'D')
 
         if direction == 'L':
             if coordinates[1] < 0:
                 sm.SERVER_TURN_RIGHT(socket)
                 if not messagesArray:
-                    receiveMessage(socket)
-                    recieveCoordinate(messagesArray.pop(0).decode())
+                    receiveMessage(socket, 'coordinates')
+                    recieveCoordinate(messagesArray.pop(0).decode(), socket)
                 else:
-                    recieveCoordinate(messagesArray.pop(0).decode())
+                    recieveCoordinate(messagesArray.pop(0).decode(), socket)
                 algorithm(socket, coordinates, 'U')
             else:
                 sm.SERVER_TURN_LEFT(socket)
                 if not messagesArray:
-                    receiveMessage(socket)
-                    recieveCoordinate(messagesArray.pop(0).decode())
+                    receiveMessage(socket, 'coordinates')
+                    recieveCoordinate(messagesArray.pop(0).decode(), socket)
                 else:
-                    recieveCoordinate(messagesArray.pop(0).decode())
+                    recieveCoordinate(messagesArray.pop(0).decode(), socket)
                 algorithm(socket, coordinates, 'D')
 
         if direction == 'U':
             if coordinates[1] < 0:
                 sm.SERVER_MOVE(socket)
                 if not messagesArray:
-                    receiveMessage(socket)
-                    coordinates = recieveCoordinate(messagesArray.pop(0).decode())
+                    receiveMessage(socket, 'coordinates')
+                    coordinates2 = recieveCoordinate(messagesArray.pop(0).decode(), socket)
                 else:
-                    coordinates = recieveCoordinate(messagesArray.pop(0).decode())
-                algorithm(socket, coordinates, direction)
+                    coordinates2 = recieveCoordinate(messagesArray.pop(0).decode(), socket)
+                if coordinates == coordinates2:
+                    direction, coordinates2 = obstacles(socket)
+                algorithm(socket, coordinates2, direction)
+                algorithm(socket, coordinates2, direction)
             else:
                 sm.SERVER_TURN_RIGHT(socket)
                 if not messagesArray:
-                    receiveMessage(socket)
-                    recieveCoordinate(messagesArray.pop(0).decode())
+                    receiveMessage(socket, 'coordinates')
+                    recieveCoordinate(messagesArray.pop(0).decode(), socket)
                 else:
-                    recieveCoordinate(messagesArray.pop(0).decode())
+                    recieveCoordinate(messagesArray.pop(0).decode(), socket)
                 sm.SERVER_TURN_RIGHT(socket)
                 if not messagesArray:
-                    receiveMessage(socket)
-                    recieveCoordinate(messagesArray.pop(0).decode())
+                    receiveMessage(socket, 'coordinates')
+                    recieveCoordinate(messagesArray.pop(0).decode(), socket)
                 else:
-                    recieveCoordinate(messagesArray.pop(0).decode())
+                    recieveCoordinate(messagesArray.pop(0).decode(), socket)
                 algorithm(socket, coordinates, 'D')
 
         if direction == 'D':
             if coordinates[1] < 0:
                 sm.SERVER_TURN_RIGHT(socket)
                 if not messagesArray:
-                    receiveMessage(socket)
-                    recieveCoordinate(messagesArray.pop(0).decode())
+                    receiveMessage(socket, 'coordinates')
+                    recieveCoordinate(messagesArray.pop(0).decode(), socket)
                 else:
-                    recieveCoordinate(messagesArray.pop(0).decode())
+                    recieveCoordinate(messagesArray.pop(0).decode(), socket)
                 sm.SERVER_TURN_RIGHT(socket)
                 if not messagesArray:
-                    receiveMessage(socket)
-                    recieveCoordinate(messagesArray.pop(0).decode())
+                    receiveMessage(socket, 'coordinates')
+                    recieveCoordinate(messagesArray.pop(0).decode(), socket)
                 else:
-                    recieveCoordinate(messagesArray.pop(0).decode())
+                    recieveCoordinate(messagesArray.pop(0).decode(), socket)
                 algorithm(socket, coordinates, 'U')
             else:
                 sm.SERVER_MOVE(socket)
                 if not messagesArray:
-                    receiveMessage(socket)
-                    coordinates = recieveCoordinate(messagesArray.pop(0).decode())
+                    receiveMessage(socket, 'coordinates')
+                    coordinates2 = recieveCoordinate(messagesArray.pop(0).decode(), socket)
                 else:
-                    coordinates = recieveCoordinate(messagesArray.pop(0).decode())
-                algorithm(socket, coordinates, direction)
+                    coordinates2 = recieveCoordinate(messagesArray.pop(0).decode(), socket)
+                if coordinates == coordinates2:
+                    direction, coordinates2 = obstacles(socket)
+                algorithm(socket, coordinates2, direction)
