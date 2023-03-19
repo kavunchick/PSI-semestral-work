fa02d7b1faab35abe2f5e1a040793a27dc702943
VladHiliaka <127258341+VladHiliaka@users.noreply.github.com>
2023-03-19 21:40:57 +0100

    Update server_message.py

--- server_message.py
+++ server_message.py
@@ -1,6 +1,4 @@
 import additional_function as ad
-import errno
-
 
 KEYS = {0: (23019, 32037), 1: (32037, 29295), 2: (18789, 13603), 3: (16443, 29533), 4: (18189, 21952)}
 
@@ -20,6 +18,15 @@ def SERVER_SYNTAX_ERROR(socket):
     socket.send(b'301 SYNTAX ERROR\a\b')
     socket.close()
 
+def SERVER_PICK_UP(socket):
+    socket.send(b'105 GET MESSAGE\a\b')
+    if not ad.messagesArray:
+        ad.receiveMessage(socket, 'SERVER_PICK_UP')
+        ad.messagesArray.pop(0)
+    else:
+        ad.messagesArray.pop(0).decode()
+    socket.send(b'106 LOGOUT\a\b')
+    socket.close()
 
 def ACCEPT_CLIENT_KEY(socket, name, key_id):
     number = (ad.hashName(name) + KEYS[key_id][1]) % 65536
@@ -28,7 +35,10 @@ def ACCEPT_CLIENT_KEY(socket, name, key_id):
         clientID = ad.messagesArray.pop(0).decode()
     else:
         clientID = ad.messagesArray.pop(0).decode()
+    if len(clientID) > 5 or ' ' in clientID:
+        SERVER_SYNTAX_ERROR(socket)
     clientID = int(clientID)
+
     if clientID == number:
         socket.send(b'200 OK\a\b')
     else:
@@ -49,8 +59,10 @@ def SERVER_TURN_RIGHT(socket):
 
 
 def ACCEPT_CLIENT_USERNAME(socket):
-    ad.receiveMessage(socket)
+    ad.receiveMessage(socket, 'ACCEPT_CLIENT_USERNAME')
     BotName = ad.messagesArray.pop(0).decode()
+    if len(BotName) > 16:
+        SERVER_SYNTAX_ERROR(socket)
     return BotName
 
 
