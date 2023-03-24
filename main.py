5d6593f7ff14eb137b039308da9c4bb352cd3425
VladHiliaka <127258341+VladHiliaka@users.noreply.github.com>
2023-03-24 14:33:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,40 +3,30 @@ import threading
 import server_message as sm
 import additional_function as ad
 
-
-print_lock = threading.Lock()
-
-HOST = "127.0.0.1"
+HOST = '127.0.0.1'
 PORT = 6000
 
 
 def connection(socket):
-    BotName_decode = sm.ACCEPT_CLIENT_USERNAME(socket)
-    KeyID = sm.SERVER_KEY_REQUEST(socket)
-    sm.SERVER_CONFIRMATION(socket, BotName_decode, KeyID)
-    sm.ACCEPT_CLIENT_KEY(socket, BotName_decode, KeyID)
-    ad.algorithm(socket)
+    try:
+        BotName_decode = sm.ACCEPT_CLIENT_USERNAME(socket)
+        KeyID = sm.SERVER_KEY_REQUEST(socket)
+        sm.SERVER_CONFIRMATION(socket, BotName_decode, KeyID)
+        sm.ACCEPT_CLIENT_KEY(socket, BotName_decode, KeyID)
+        ad.algorithm(socket)
+    except:
+        pass
 
 
 def main():
-    Lsocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
-    Lsocket.bind((HOST, PORT))
-    print(f'Lsocket binded to {HOST, PORT}')
-    Lsocket.listen()
-    print(f'Listen {HOST, PORT}')
-    while True:
-        NewSocket, address = Lsocket.accept()
-        print_lock.acquire()
-        print('Connected to :', address[0], ':', address[1])
-        try:
+    with socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM) as Lsocket:
+        Lsocket.bind((HOST, PORT))
+        Lsocket.listen()
+        while True:
+            NewSocket, address = Lsocket.accept()
             thread = threading.Thread(target=connection, args=(NewSocket,))
             thread.start()
-        except Exception as e:
-            print(e)
-        finally:
-            print_lock.release()
-    Lsocket.close()
 
 
-
-main()
+if __name__ == '__main__':
+    main()
